d8b852ffb9865018f2f665fb767421f760bcbd86
Valmir Jr <junior.valmir6@gmail.com>
2023-02-17 00:23:28 -0300

    Update ExemploNormaEuclidiana.py

--- ExemploNormaEuclidiana.py
+++ ExemploNormaEuclidiana.py
@@ -9,3 +9,18 @@ import cupy as cp
 
 x_gpu = cp.array([1, 2, 3])
 norma = cp.linalg.norm(x_gpu)
+
+# Transferring array from CPU memory to GPU memory
+x_gpu = cp.asarray(x_cpu)
+norma = cp.linalg.norm(x_gpu)
+
+# Customized kernel with CuPy
+kernel_norma = cp.ReductionKernel(
+    'float32 x',  # Parametros de entrada
+    'float32 y',  # Parametros de saida
+    'x * x',  # Operacao aplicada a cada elemento
+    'a + b',  # Operacao de reducao
+    'y = sqrt(a)',  # Operacao final
+    '0',  # Valor identidade
+    'norma'  # Nome do kernel
+)
